4f8872286e80d58f90050c912b101720acc9c3ec
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-04-01 20:28:41 +0700

    add post-method to contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . apps import CatalogConfig
+
 from . import views
+from .apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-path('contacts/', views.contacts, name='contacts'),
+    path("", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
