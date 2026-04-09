c191a6a466229e62e74b2036e58640f2c6ae9509
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-04-09 15:09:46 +0700

    add create_product, create_category pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
     path("product/<int:pk>/", views.product, name="product"),
+    path("select_category/", views.select_category, name="select_category"),
+    path("create_category/", views.create_category, name="create_category"),
+    path("create_product/<int:cat_id>/", views.create_product, name="create_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from .models import Contact, Product
+from .models import Category, Contact, Product
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -29,3 +32,41 @@ def product(request: HttpRequest, pk: int) -> HttpResponse:
     """Контроллер страницы определенного продукта"""
 
     return render(request, "product.html", context={"product": Product.objects.get(id=pk)})
+
+
+def select_category(request: HttpRequest) -> HttpResponse:
+    """Контроллер страницы выбора категории размещаемого товара"""
+
+    return render(request, "select_category.html", context={"categories": Category.objects.all()})
+
+
+def create_product(request: HttpRequest, cat_id: int) -> HttpResponse:
+    """Контроллер страницы размещения новых продуктов"""
+
+    current_category = Category.objects.get(id=cat_id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        path_to_photo = None
+        if request.POST.get("photo"):
+            file = request.FILES["photo"]
+            if isinstance(file, UploadedFile):
+                default_storage.save(f"images/{file.name}", ContentFile(file.read()))
+                path_to_photo = f"images/{file.name}"
+        price = request.POST.get("price")
+        Product.objects.create(
+            name=name, description=description, photo=path_to_photo, price=price, category=current_category
+        )
+        return render(request, "successful_sending.html")
+    return render(request, "create_product.html", context={"category": current_category})
+
+
+def create_category(request: HttpRequest) -> HttpResponse:
+    """Контроллер страницы создания новой категории продуктов"""
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        Category.objects.create(name=name, description=description)
+        return redirect("/select_category/")
+    return render(request, "create_category.html")
