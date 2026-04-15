ad922e985e2f043c46166333f8b1028ba7cdbd04
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-04-16 02:17:53 +0700

    some fixes for sky_blog views

--- sky_blog/views.py
+++ sky_blog/views.py
@@ -1,3 +1,6 @@
+from typing import Any, Optional
+
+from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.urls import reverse_lazy
@@ -13,11 +16,24 @@ class BlogHome(ListView):
     paginate_by = 8
     ordering = ["-created_at"]
 
+    def get_queryset(self) -> QuerySet:
+        """Метод, допускающий к отображению только те статьи, которые предназначены для публикации"""
+
+        return super().get_queryset().filter(is_published=True)
+
 
 class ArticleDetailView(DetailView):
     """Контроллер страницы статьи блога"""
 
     model = Article
+
+    def get_object(self, queryset: Optional[QuerySet] = None) -> Any:
+        """Метод, увеличивающий счетчик просмотров статьи при посещении ее страницы"""
+
+        article = super().get_object(queryset)
+        article.views_count += 1
+        article.save()
+        return article
 
 
 class CreateArticle(CreateView):
@@ -33,7 +49,11 @@ class UpdateArticle(UpdateView):
 
     model = Article
     fields = ("title", "content", "preview", "is_published")
-    success_url = reverse_lazy("sky_blog:blog", kwargs={"page": 1})
+
+    def get_success_url(self) -> Any:
+        """Метод получения url статьи для перехода после ее редактирования"""
+
+        return reverse_lazy("sky_blog:article", kwargs={"pk": self.object.pk})
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         """Метод, проверяющий необходимость удалить существующее в статье изображение"""
