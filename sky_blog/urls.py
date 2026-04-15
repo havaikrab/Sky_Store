9b3735dd26e6152ba0ebba7e908de852fe4a9136
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-04-16 01:02:01 +0700

    add cbv-crud controllers for app sky_blog

--- sky_blog/urls.py
+++ sky_blog/urls.py
@@ -8,4 +8,7 @@ app_name = SkyBlogConfig.name
 urlpatterns = [
     path("blog/page/<int:page>/", views.BlogHome.as_view(), name="blog"),
     path("article/<int:pk>/", views.ArticleDetailView.as_view(), name="article"),
+    path("create_article/", views.CreateArticle.as_view(), name="create_article"),
+    path("article/update/<int:pk>/", views.UpdateArticle.as_view(), name="update_article"),
+    path("article/delete/<int:pk>/", views.DeleteArticle.as_view(), name="delete_article"),
 ]
--- sky_blog/views.py
+++ sky_blog/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import DetailView, ListView
+from django.forms import BaseModelForm
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from .models import Article
 
@@ -8,9 +11,50 @@ class BlogHome(ListView):
 
     model = Article
     paginate_by = 8
+    ordering = ["-created_at"]
 
 
 class ArticleDetailView(DetailView):
     """Контроллер страницы статьи блога"""
 
     model = Article
+
+
+class CreateArticle(CreateView):
+    """Контроллер страницы написания новой статьи"""
+
+    model = Article
+    fields = ("title", "content", "preview", "is_published")
+    success_url = reverse_lazy("sky_blog:blog", kwargs={"page": 1})
+
+
+class UpdateArticle(UpdateView):
+    """Контроллер страницы редактирования существующей статьи"""
+
+    model = Article
+    fields = ("title", "content", "preview", "is_published")
+    success_url = reverse_lazy("sky_blog:blog", kwargs={"page": 1})
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """Метод, проверяющий необходимость удалить существующее в статье изображение"""
+
+        delete_preview = self.request.POST.get("delete_image")
+        if delete_preview is not None:
+            self.object.preview.delete(save=False)
+            self.object.preview = None
+        return super().form_valid(form)
+
+
+class DeleteArticle(DeleteView):
+    """Контроллер удаления статьи"""
+
+    model = Article
+    success_url = reverse_lazy("sky_blog:blog", kwargs={"page": 1})
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """Метод, проверяющий необходимость удалить существующее в статье изображение"""
+
+        if self.object.preview is not None:
+            self.object.preview.delete(save=False)
+            self.object.preview = None
+        return super().form_valid(form)
